cd89fac2543c5b989a709ce59e2224ce36e693ab
Kirill_Markov <enifl33fi@gmail.com>
2023-11-10 00:47:07 +0300

    repository id type fix

--- src/main/java/com/enifl33fi/lab4_backend/api/repository/RefreshTokenRepository.java
+++ src/main/java/com/enifl33fi/lab4_backend/api/repository/RefreshTokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
 
     void deleteByToken(String token);
